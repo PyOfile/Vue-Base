16d81d66d9a01a860a1765d42a391c67427ad5c4
Boyd G <49822283+PyOfile@users.noreply.github.com>
2020-04-16 12:59:09 -0700

    Update Mothman.vue

--- src/components/Mothman.vue
+++ src/components/Mothman.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p> This site is dedicated to reaching out to small businesses that would like to build a legit web 
-        presence no matter your technical level. We want to bring the Mom & Pop business to the web. Our 
-        objective is to offer you a site that pleases you with services available to help with site management. 
-        We want to bring the new wave of companies to the web. Why? Because we want to bring small business 
-        into the 21st with a helping hand. So please contact us for a consultation.
+    <p> 
+    
     </p>
   </div>
 </template>
